0050303a89559ff85a1b49fdf0ac9c35a90a55f1
user <user@DESKTOP-COJ9858>
2022-06-20 18:34:25 +0900

    1111

--- GitHubProject2/src/com/sist/MainClass.java
+++ GitHubProject2/src/com/sist/MainClass.java
@@ -5,6 +5,11 @@ public class MainClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello GitHub");
+		System.out.println("Hello GitHub");
+		System.out.println("Hello GitHub");
+		System.out.println("Hello GitHub");
+		System.out.println("Hello GitHub");
+		System.out.println("Hello GitHub");
 	}
 
 }
